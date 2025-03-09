078dd3209d7dc9a639472a65db05802949de422b
Alisson L B Rocha <40269909+AlissonLb@users.noreply.github.com>
2025-03-09 05:35:37 -0300

    Add files via upload

---
+++ LeitorDeTexto/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string arquivo = args[0];
+        string line = "";
+
+        try
+        {
+            StreamReader ler = new StreamReader(arquivo);
+            line = ler.ReadLine();
+            while (line != null)
+            {
+                Console.WriteLine(line);
+                line = ler.ReadLine();
+            }
+            ler.Close();
+            Console.ReadLine();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro {e.Message}");
+        }
+    }
+}
